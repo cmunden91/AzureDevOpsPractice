61ae0206f88bd1cd54a07b85a43839f3b19f95ee
Christopher Munden <cmunden@alumni.unca.edu>
2023-10-30 23:35:46 -0400

    Added build defintion

--- IaC/Terraform/main.tf
+++ IaC/Terraform/main.tf
@@ -21,7 +21,15 @@ resource "azuredevops_project" "androidProject" {
   name        = "Azure Android Project"
   description = "This project will simulate an andriod DevOps enviorment."
   visibility         = "private"
-  version_control    = "Git"
+}
+
+resource "azuredevops_project_features" "androidProjectFeatures" {
+  project_id = azuredevops_project.androidProject.id
+  features = {
+    "repositories" = "enabled"
+    "artifacts" = "enabled"
+    "pipelines" = "enabled"
+  }
 }
 
 resource "azuredevops_git_repository" "androidRepo" {
@@ -44,5 +52,33 @@ resource "azuredevops_git_repository_branch" "sit_branch" {
 resource "azuredevops_git_repository_branch" "dut_branch" {
   repository_id = azuredevops_git_repository.androidRepo.id
   name          = "dut_branch"
-  ref_branch    = "refs/heads/sit_branch"
+  ref_branch    = "refs/heads/main"
+}
+
+resource "azuredevops_build_definition" "prod_build" {
+  project_id = azuredevops_project.androidProject.id
+  name       = "Production Build Pipeline"
+
+  ci_trigger {
+    use_yaml = false
+  }
+
+  schedules {
+    branch_filter {
+      include = ["main"]
+      exclude = ["sit_branch", "dut_branch"]
+    }
+    days_to_build              = ["Wed", "Sun"]
+    schedule_only_with_changes = true
+    start_hours                = 10
+    start_minutes              = 59
+    time_zone                  = "(UTC) Coordinated Universal Time"
+  }
+
+  repository {
+    repo_type   = "TfsGit"
+    repo_id     = azuredevops_git_repository.androidRepo.id
+    branch_name = azuredevops_git_repository.androidRepo.default_branch
+    yml_path    = "azure-pipelines.yml"
+  }
 }
